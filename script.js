72be410fa69c27ce32ee050f1374cb86b54760b3
Mohamad Ali Hamed <mohalihamed1@gmail.com>
2025-11-09 19:38:37 +0200

    Second

---
+++ script.js
@@ -0,0 +1,13 @@
+const press_add = document.getElementById("add");
+const input_add = document.getElementById("add_input");
+const list_add = document.getElementsByClassName("list");
+const feedback = document.getElementById("feedback)");
+
+
+function buttonpressed(){
+    list_add=[]
+    if (input_add.value == ""){
+        feedback.innerText= "You have an empty input"}
+        else {list_add.value}
+    }
+        
